e6d54e5e78caf4f82e7ad6bb84a99c489263b4a6
night0hunter <74367739+night0hunter@users.noreply.github.com>
2020-11-13 09:53:23 +0300

    hashCode for paswword added

--- widgets/auth.py
+++ widgets/auth.py
@@ -5,8 +5,7 @@ import sqlite3
 from PyQt5.QtWidgets import QLineEdit
 import cfg
 from service.service import create_log
-
-# DB_NAME = "cash_machine.db"
+import hashlib
 
 
 class Authorization(QtWidgets.QWidget):
@@ -17,15 +16,16 @@ class Authorization(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         uic.loadUi('./ui/authorization.ui', self)
         self.auth_btn_2.clicked.connect(self.auth)
-        self.hide_password.clicked.connect(self.hidePassword)
-        self.password_value.setEchoMode(QLineEdit.Password)
+        # self.hide_password.clicked.connect(self.hidePassword)
+        # self.password_value.setEchoMode(QLineEdit.Password)
 
     def auth(self):
         con = sqlite3.connect(cfg.DB_NAME)
         cur = con.cursor()
+        hash_password = hashlib.sha512(self.password_value.text().encode("utf-8")).hexdigest()
         data = {
             "login": self.login_value.text(),
-            "password": self.password_value.text()
+            "password": hash_password
         }
         if data["login"] and data["password"]:
             result = cur.execute(
@@ -50,9 +50,9 @@ class Authorization(QtWidgets.QWidget):
             self.error2.setText(
                 "Для успешной авторизации\nнеобходимо заполнить все поля!")
 
-    def hidePassword(self):
-        print(self.password_value.EchoMode())
-        if self.password_value.EchoMode() == 2:
-            self.password_value.setEchoMode(QLineEdit.Normal)
-        elif self.password_value.EchoMode() == 0:
-            self.password_value.setEchoMode(QLineEdit.Password)
+    # def hidePassword(self):
+    #     print(self.password_value.EchoMode())
+    #     if self.password_value.EchoMode() == 2:
+    #         self.password_value.setEchoMode(QLineEdit.Normal)
+    #     elif self.password_value.EchoMode() == 0:
+    #         self.password_value.setEchoMode(QLineEdit.Password)
